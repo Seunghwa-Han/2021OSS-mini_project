f642aa44f11462b7a6a41ecd87bd02e674cd8d6d
SH <22000786@handong.edu>
2021-04-14 08:41:27 +0900

    fixing error

--- product.h
+++ product.h
@@ -13,7 +13,7 @@ void read(Product p); //하나의 제품을 출력 함수 R
 int update(Product * p); //제품 수정하는 함수 U, 정상적으로 수정되었으면 1을 리턴 
 int deleteProduct(Product * p); //제품 삭제하는 함수 D, 정상적으로 삭제되었으면 1을 리턴 
 
-void savaData(Product * p[], int count); //file에 저장하는 함수
+void saveData(Product * p[], int count); //file에 저장하는 함수
 int loadData(Product * p[]); //file에서 데이터 불러오는 함수, 데이터 개수(product 개수)를 리턴 
 
 void searchName(Product * p[], int count, char s_name[]);   //이름을 검색하는 함수 
